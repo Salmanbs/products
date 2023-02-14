9eeb5bf8396c8a275b9523bfbdc304620af35993
Salmanbs <salmanbinsalahudeen@gmail.com>
2023-02-14 18:30:25 +0530

    version1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import data from "./products.json";
+import Product from './Product';
+
+function App() {
+  // data.products.map(item => {console.log(item.title)})
+  return (
+   <Product/>
+  );
+}
+
+export default App;
